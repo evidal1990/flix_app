bb2a4cff9effaa5babc6b63e24de773bd96dec44
evidal1990 <ecsr.vidal@gmail.com>
2024-12-22 20:59:42 -0300

    Inserindo serviço de login

---
+++ api/service.py
@@ -0,0 +1,16 @@
+import requests
+
+
+class AuthenticationService:
+
+    def __init__(self):
+        self.__base_url = "https://evidal1990.pythonanywhere.com/api/v1/"
+        self.__auth_url = f"{self.__base_url}authentication/token"
+
+    def get_token(self, username, password):
+        response = requests.post(
+            self.__auth_url, data={"username": username, "password": password}
+        )
+        if response.status_code == 200:
+            return response.json()
+        return {"error": f"Erro ao autenticar. Status Code {response.status_code}"}
---
+++ login/service.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from api.service import AuthenticationService
+
+
+def login(username, password):
+    authentication_service = AuthenticationService()
+    response = authentication_service.get_token(username, password)
+    error = response.get("error")
+    if error:
+        st.error(f'Falha ao realizar login: {error}')
+    else:
+        st.session_state.token = response.get("access")
