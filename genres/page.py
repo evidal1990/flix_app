7945ba9c5c685d6c89d8689793e7c3f3ea14a671
evidal1990 <ecsr.vidal@gmail.com>
2024-12-28 23:44:07 -0300

    Adicionando containers à página de gêneros e criando repository e service de movies

--- genres/page.py
+++ genres/page.py
@@ -6,24 +6,26 @@ from genres.service import GenreService
 
 def show_genres():
     genre_service = GenreService()
-    genres = genre_service.get_genres()
 
-    if genres:
-        st.title("Lista de gêneros")
-        AgGrid(
-            data=pd.DataFrame(pd.json_normalize(genres)),
-            reload_data=True,
-            key="genres_grid"
-        )
-    else:
-        st.warning("Nenhum gênero cadastrado")
-
-    st.title("Cadastrar novo gênero")
-    genre_name = st.text_input("Nome do gênero")
-    if st.button("Cadastrar"):
-        new_genre = genre_service.create_genre(name=genre_name)
-        if new_genre:
-            st.success(f'Gênero "{genre_name}" cadastrado com sucesso')
-            st.rerun()
+    with st.container():
+        genres = genre_service.get_genres()
+        if genres:
+            st.title("Lista de gêneros")
+            AgGrid(
+                data=pd.DataFrame(pd.json_normalize(genres)),
+                reload_data=True,
+                key="genres_grid"
+            )
         else:
-            st.error(f'Erro ao cadastrar gênero "{genre_name}"')
+            st.warning("Nenhum gênero cadastrado")
+
+    with st.container():
+        st.title("Cadastrar novo gênero")
+        genre_name = st.text_input("Nome do gênero")
+        if st.button("Cadastrar"):
+            new_genre = genre_service.create_genre(name=genre_name)
+            if new_genre:
+                st.success(f'Gênero "{genre_name}" cadastrado com sucesso')
+                st.rerun()
+            else:
+                st.error(f'Erro ao cadastrar gênero "{genre_name}"')
