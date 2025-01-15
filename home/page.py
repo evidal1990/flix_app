9d81da4c39c75aa7006b42cf157ea05b39d0f660
evidal1990 <ecsr.vidal@gmail.com>
2025-01-15 12:34:19 -0300

    Adicionando gráfico de filmes por gênero e estatísticas de filmes na página home (parcial)

--- home/page.py
+++ home/page.py
@@ -1,5 +1,35 @@
 import streamlit as st
+import plotly.express as px
+from movies.service import MovieService
 
 
 def show_home():
+    movie_service = MovieService()
+    movie_stats = movie_service.get_movie_stats()
+
     st.title("Estatística de filmes")
+
+    print(movie_stats)
+
+    if len(movie_stats["movies_by_genre"]) > 0:
+        with st.container():
+            st.subheader("Filmes por gênero")
+            fig = px.pie(
+                movie_stats["movies_by_genre"],
+                values="count",
+                names="genre__name",
+                title="Filmes por gênero"
+            )
+            st.plotly_chart(fig)
+
+    with st.container():
+        st.subheader("Total de filmes cadastrados")
+        st.write(movie_stats["movies_total"])
+
+    with st.container():
+        st.subheader("Total de avaliações cadastradas")
+        st.write(movie_stats["movies_reviews"])
+
+    with st.container():
+        st.subheader("Média geral de estrelas nas avaliações")
+        st.write(movie_stats["movies_avg_stars"])
