1a6bd75aa687b87aa6919b5f49fcfa6cb498cd31
evidal1990 <ecsr.vidal@gmail.com>
2025-01-16 09:06:26 -0300

    Adicionando cache ao app de actors

--- actors/service.py
+++ actors/service.py
@@ -1,4 +1,5 @@
 from actors.repository import ActorRepository
+import streamlit as st
 
 
 class ActorService:
@@ -6,11 +7,21 @@ class ActorService:
         self.actor_repository = ActorRepository()
 
     def get_actors(self):
-        return self.actor_repository.get_actors()
+        if "actors" in st.session_state:
+            return st.session_state.actors
+        actors = self.actor_repository.get_actors()
+        st.session_state.actors = actors
+        return actors
 
     def create_actor(self, name, birthday, nationality):
-        actor = dict(name=name, birthday=birthday, nationality=nationality)
-        return self.actor_repository.create_actor(actor)
+        actor = dict(
+            name=name,
+            birthday=birthday,
+            nationality=nationality
+        )
+        new_actor = self.actor_repository.create_actor(actor)
+        st.session_state.actors.append(new_actor)
+        return new_actor
 
     def get_nationalities(self):
         nationalities = self.actor_repository.get_nationalities()["nationalities"]
